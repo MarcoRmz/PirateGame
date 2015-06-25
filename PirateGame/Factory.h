c34c33e57cfa71fe3e50b65e33f6657753b573b3
MarcoRmz <marcormz19@gmail.com>
2015-06-25 11:45:21 -0500

    added factory class

---
+++ PirateGame/Factory.h
@@ -0,0 +1,14 @@
+//
+//  Factory.h
+//  PirateGame
+//
+//  Created by Marco Ramirez on 6/25/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Tile.h"
+
+@interface Factory : NSObject
+
+@end
--- PirateGame/Factory.m
+++ PirateGame/Factory.m
@@ -1,17 +1,13 @@
 //
-//  ViewController.h
+//  Factory.m
 //  PirateGame
 //
 //  Created by Marco Ramirez on 6/25/15.
 //  Copyright (c) 2015 Marco Ramirez. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "Character.h"
 #import "Factory.h"
 
-@interface ViewController : UIViewController
-
+@implementation Factory
 
 @end
-
