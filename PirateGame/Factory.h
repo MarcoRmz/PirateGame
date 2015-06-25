59f122267a959b8d9f7685ecd0164878e98a2698
MarcoRmz <marcormz19@gmail.com>
2015-06-25 18:49:00 -0500

    added arrows in storyboard and return method for tiles in factory class

--- PirateGame/Factory.h
+++ PirateGame/Factory.h
@@ -11,4 +11,6 @@
 
 @interface Factory : NSObject
 
+-(NSArray *) tiles;
+
 @end
--- PirateGame/Factory.m
+++ PirateGame/Factory.m
@@ -7,7 +7,32 @@
 //
 
 #import "Factory.h"
+#import "Tile.h"
 
 @implementation Factory
+
+-(NSArray *) tiles {
+    Tile *firstTile = [[Tile alloc] init];
+    Tile *secondTile = [[Tile alloc] init];
+    Tile *thirdTile = [[Tile alloc] init];
+    Tile *fourthTile = [[Tile alloc] init];
+    Tile *fifthTile = [[Tile alloc] init];
+    Tile *sixthTile = [[Tile alloc] init];
+    Tile *seventhTile = [[Tile alloc] init];
+    Tile *eightTile = [[Tile alloc] init];
+    Tile *ninthTile = [[Tile alloc] init];
+    Tile *tenthTile = [[Tile alloc] init];
+    Tile *eleventhTile = [[Tile alloc] init];
+    Tile *twelveTile = [[Tile alloc] init];
+    
+    NSArray *firstRow = [[NSArray alloc] initWithObjects:firstTile, secondTile, thirdTile, nil];
+    NSArray *secondRow = [[NSArray alloc] initWithObjects:fourthTile, fifthTile, sixthTile, nil];
+    NSArray *thirdRow = [[NSArray alloc] initWithObjects:seventhTile, eightTile, ninthTile, nil];
+    NSArray *fourthRow = [[NSArray alloc] initWithObjects:tenthTile, eleventhTile, twelveTile, nil];
+    
+    NSArray *tiles = [[NSArray alloc] initWithObjects:firstRow, secondRow, thirdRow, fourthRow, nil];
+    
+    return tiles;
+}
 
 @end
